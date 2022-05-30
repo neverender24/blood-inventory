3bb86c8c50331b599761727c637dc500f0e67671
George <neverender24@gmail.com>
2022-05-30 08:42:51 +0800

    check media

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -71,10 +71,6 @@
     </div>
     <!-- page-body-wrapper ends -->
   </div>
-  <script src="{{ mix('js/app.js') }}"></script>
-  <script src="{{ asset('js/off-canvas.js') }}"></script>
-  <script src="{{ asset('js/hoverable-collapse.js') }}"></script>
-  <script src="{{ asset('js/misc.js') }}"></script>
 </body>
 
 </html>
