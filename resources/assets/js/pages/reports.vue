b9bb3f54c415d5e81e766776cbc4e44c0511446f
George <neverender24@gmail.com>
2023-06-29 15:06:36 +0800

    fix report

--- resources/assets/js/pages/reports.vue
+++ resources/assets/js/pages/reports.vue
@@ -8,7 +8,7 @@
         
         <!-- Modal -->
         <div class="modal fade" id="modal_print_cafoa" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-dialog-lg" role="document">
             <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel"><span class="fa fa-print"></span> PRINT</h5>
@@ -19,7 +19,10 @@
                 <button class="btn btn-primary" @click="display_report1()" v-if="report == 1">Display</button>
                 <button class="btn btn-primary" @click="display_report2()" v-if="report == 2">Display</button>
                 <button class="btn btn-primary" @click="display_bm6()" v-if="report == 3">Display</button>
-            </div> 
+            </div>
+            <div id="content">
+
+            </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-danger btn-sm" @click="close()">Close</button>
             </div>
@@ -35,6 +38,12 @@
 .custom-text:hover {
     text-decoration: underline;
 }
+
+.modal-dialog {
+    max-width: 1080px !important;
+    margin: 30px auto;
+    height: 600px !important;
+}
 </style>
 <script>
 export default {
@@ -47,10 +56,10 @@ export default {
     methods: {
         display_report1() {
             var url =
-                "http://122.54.19.171:8080/jasperserver/flow.html?pp=u%3DJamshasadid%7Cr%3DManager%7Co%3DEMEA,Sales%7Cpa1%3DSweden&" +
+                "http://122.53.120.27:8080/jasperserver/flow.html?pp=u%3DJamshasadid%7Cr%3DManager%7Co%3DEMEA,Sales%7Cpa1%3DSweden&" +
                 "flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2Freports%2Fblood_inventory%2Fblood_report1_files&reportUnit=%2Freports%2Fblood_inventory%2Fblood_report1_files%2Fblood_report1_files&standAlone=true" +
-                "&decorate=no" +
-                "&year="+this.year;
+                "&decorate=no&output=pdf" +
+                "&year="+this.year; 
 
             window.open(
               url,
@@ -60,9 +69,9 @@ export default {
 
         display_report2() {
             var url =
-                "http://122.54.19.171:8080/jasperserver/flow.html?pp=u%3DJamshasadid%7Cr%3DManager%7Co%3DEMEA,Sales%7Cpa1%3DSweden&" +
+                "http://122.53.120.27:8080/jasperserver/flow.html?pp=u%3DJamshasadid%7Cr%3DManager%7Co%3DEMEA,Sales%7Cpa1%3DSweden&" +
                 "flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2Freports%2Fblood_inventory&reportUnit=%2Freports%2Fblood_inventory%2Fblood_report2_files&standAlone=true" +
-                "&decorate=no" +
+                "&decorate=no&output=pdf" +
                 "&year="+this.year;
 
             window.open(
@@ -73,15 +82,13 @@ export default {
 
         display_bm6() {
             var url =
-                "http://122.54.19.171:8080/jasperserver/flow.html?pp=u%3DJamshasadid%7Cr%3DManager%7Co%3DEMEA,Sales%7Cpa1%3DSweden&" +
-                "flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2Freports%2Fblood_inventory&reportUnit=%2Freports%2Fblood_inventory%2Fbm6&standAlone=true" +
-                "&decorate=no" +
+                "http://122.53.120.27:8080/jasperserver/flow.html?pp=u%3DJamshasadid%7Cr%3DManager%7Co%3DEMEA,Sales%7Cpa1%3DSweden&" +
+                "flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2Freports%2Fblood&reportUnit=%2Freports%2Fblood%2Fbm6&standAlone=true" +
+                "&decorate=no&output=pdf&download=false" +
                 "&year="+this.year;
 
-            window.open(
-              url,
-              '_blank'
-            );
+
+            $('#content').html("<iframe name='report-frame' src='"+url+"' height='600' width=100% > </iframe>")
         },
 
         print_report1() {
